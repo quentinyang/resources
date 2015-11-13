ab26c5f705f1a7a48564974d91a3a8e3665be27f
Quentin Yang <quentinyang1985@gmail.com>
2015-11-13 12:29:29 +0800

    Demo 3: compile and lnk

--- angularjs/js/directive/filter.js
+++ angularjs/js/directive/filter.js
@@ -2,8 +2,12 @@ var appModule = angular.module('app', []);
 appModule.directive('filter', function() {
     return {
         restrict: 'ECMA',
-        template: '<div>Hi i am a filter2 box.<p ng-transclude></p></div>',
+        template: '<div>Hi i am a filter box.<p ng-transclude></p></div>',
         transclude: true,
         replace: true
     };
+});
+
+appModule.controller('MyCtrl', function($scope) {
+    $scope.filters = ['price','area','district','community','room'];
 });
