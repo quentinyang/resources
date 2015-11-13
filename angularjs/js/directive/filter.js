d3cd24a0e08090e568f947e4c68351b76482537f
Quentin Yang <quentinyang1985@gmail.com>
2015-11-13 12:23:58 +0800

    Demo 2: directive transclude

--- angularjs/js/directive/filter.js
+++ angularjs/js/directive/filter.js
@@ -2,7 +2,8 @@ var appModule = angular.module('app', []);
 appModule.directive('filter', function() {
     return {
         restrict: 'ECMA',
-        template: '<div>Hi i am a filter2 box.</div>',
+        template: '<div>Hi i am a filter2 box.<p ng-transclude></p></div>',
+        transclude: true,
         replace: true
     };
 });
